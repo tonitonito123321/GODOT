5ba04cb880b2a5d3ae6d0a736370d0c912162b5f
tonitonito123321 <5586996@alu.murciaeduca.es>
2025-01-28 19:36:57 +0100

    JUEGO MENU FINAL

--- MI JUEGO/SCRIPTS/MENU.gd
+++ MI JUEGO/SCRIPTS/MENU.gd
@@ -1,8 +1,13 @@
 extends Control
 
-
+@onready var start: Button = $BOTONES/Start
+@onready var setting: Button = $BOTONES/Setting
+@onready var exit: Button = $BOTONES/EXIT
+@onready var sonido_boton_puru: AudioStreamPlayer = $BOTONES/Start/SonidoBotonPuru
+@onready var sonido_boton_2: AudioStreamPlayer = $BOTONES/Start/SonidoBoton2
 
 func _on_start_pressed():
+	$BOTONES/Start/SonidoBoton2.play()
 	get_tree().change_scene_to_file("res://juego.tscn")
 
 
