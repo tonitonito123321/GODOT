1e0b7104ac2af11d8be314d92f737814c7464053
tonitonito123321 <5586996@alu.murciaeduca.es>
2025-01-28 14:33:27 +0100

    juego bueno

--- MI JUEGO/SCRIPTS/SETTING.gd
+++ MI JUEGO/SCRIPTS/SETTING.gd
@@ -3,4 +3,12 @@ extends Control
 
 
 func _on_volver_pressed():
-	get_tree().change_scene_to_file("res://SETTING.tscn")
+	get_tree().change_scene_to_file("res://MENU.tscn")
+
+
+func _on_instrucciones_pressed():
+	get_tree().change_scene_to_file("res://INSTRUCCIONES.tscn")
+
+
+func _on_creditos_pressed():
+	get_tree().change_scene_to_file("res://CREDITS.tscn")
